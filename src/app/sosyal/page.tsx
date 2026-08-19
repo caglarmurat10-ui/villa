c2f729f9486e799ba20204a555479edf0c036016
caglarmurat10-ui <caglarmurat10@gmail.com>
2026-08-19 23:39:46 +0300

    Show Meta account connections in social dashboard

--- src/app/sosyal/page.tsx
+++ src/app/sosyal/page.tsx
@@ -1,8 +1,11 @@
+import MetaConnections from "@/components/MetaConnections";
 import SocialMediaView from "@/components/SocialMediaView";
+import { listMetaAccounts } from "@/lib/meta-store";
 import { listSocialPosts } from "@/lib/social-db";
 
 export const dynamic = "force-dynamic";
 
 export default async function SocialPage() {
-  return <SocialMediaView initialPosts={await listSocialPosts()} />;
+  const [posts, accounts] = await Promise.all([listSocialPosts(), listMetaAccounts()]);
+  return <><MetaConnections initialAccounts={accounts} /><SocialMediaView initialPosts={posts} /></>;
 }
