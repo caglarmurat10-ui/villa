cbac4f0cba6ce774eef10535988630e34ef146eb
caglarmurat10-ui <caglarmurat10@gmail.com>
2026-08-28 17:49:37 +0300

    Add social brand settings page

---
+++ src/app/sosyal/marka/page.tsx
@@ -0,0 +1,7 @@
+import BrandProfileCenter from "@/components/BrandProfileCenter";
+
+export const dynamic = "force-dynamic";
+
+export default function BrandPage() {
+  return <BrandProfileCenter />;
+}
