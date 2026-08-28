00d79baf4a38897f909a19514494a4e9c2f68d06
caglarmurat10-ui <caglarmurat10@gmail.com>
2026-08-28 20:49:18 +0300

    Expose Facebook brand automation on brand page

--- src/app/sosyal/marka/page.tsx
+++ src/app/sosyal/marka/page.tsx
@@ -1,9 +1,13 @@
 import BrandProfileCenter from "@/components/BrandProfileCenter";
+import FacebookBrandAutomation from "@/components/FacebookBrandAutomation";
 import { listMetaAccounts } from "@/lib/meta-store";
 
 export const dynamic = "force-dynamic";
 
 export default async function BrandPage() {
   const accounts = await listMetaAccounts();
-  return <BrandProfileCenter accounts={accounts} />;
+  return <>
+    <FacebookBrandAutomation accounts={accounts} />
+    <BrandProfileCenter accounts={accounts} />
+  </>;
 }
